bb04098dc41f693cbf600ec0e53df90dece8498f
dadadasouta <okasou.soccer@gmail.com>
2022-09-29 12:09:15 +0900

    アクティブハッシュの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :day_id, :category_id, :prefecture_id, :delivery_charge_defrayer_id, :status_id, :description, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :delivery_charge_defrayer
+    belongs_to :prefecture
+    belongs_to :day
 
   validates :name,                        presence: true
-  validates :price,                       presence: true
-  validates :descryption,                 presence: true
-  validates :status_id,                   presence: true
-  validates :delivery_charge_defrayer_id, presence: true
-  validates :day_id,                      presence: true
-  validates :prefecture_id,               presence: true
-  validates :category_id,                 presence: true
-  
+  validates :descryption,                 presence: true, length: { minimum: 1, maximum: 1000 }
+  validates :image,                       presence: true
+  validates :category_id,                 numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id,                   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_defrayer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,               numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id,                      numericality: { other_than: 1 , message: "can't be blank"}
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message: "can't be blank" }
+  end
   
 end
