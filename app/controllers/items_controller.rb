e91cd7bdcc4e9708e1029f954953aaf12ffa1c30
dadadasouta <okasou.soccer@gmail.com>
2022-10-02 18:46:03 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -17,7 +17,7 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   private
 
   def item_params
