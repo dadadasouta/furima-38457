14d71d1445d4f1c1192439c4cff12ef6c99618fa
dadadasouta <okasou.soccer@gmail.com>
2022-10-04 18:27:48 +0900

    rubocopの実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,12 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new(order_params)
   end
+
+  private
+
+  def order_params
+    params.permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id).merge(
+      token: params[:token], user_id: current_user.id
+    )
+  end
 end
