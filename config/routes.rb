e2fc10f88b9bf881af9f18b4e27082d8011993df
dadadasouta <okasou.soccer@gmail.com>
2022-10-04 17:57:42 +0900

    アクションの記入

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :items, only:[:index, :new, :create, :show, :edit, :update, :destroy]
 
   resources :users, only: [:edit, :update]
+  resources :orders, only: [:index, :create]
 end
