b1899af35f4efc735ce5db6cbeea76498609afd7
dadadasouta <okasou.soccer@gmail.com>
2022-10-05 16:59:01 +0900

    クレジットカード利用機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index]
+
   def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
     @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
@@ -9,5 +22,18 @@ class OrdersController < ApplicationController
     params.permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id).merge(
       token: params[:token], user_id: current_user.id
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: order_params[:price],  
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,4 @@
-class orderAddress
+class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :user_id, :token
 
