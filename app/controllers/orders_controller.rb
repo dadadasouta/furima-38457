34847ecc272d0c48da5ca8a0549f5d83ad4cb616
dadadasouta <okasou.soccer@gmail.com>
2022-10-07 16:42:17 +0900

    テストコードの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    @order_address = OrderAddress.new(order_params)
+    @order_address = OrderAddress.new
   end
 
   def create
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,7 +4,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :city
     validates :addresses
     validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
     validates :item_id
